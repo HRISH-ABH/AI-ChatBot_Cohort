a1017f1f997bf4d84a00f7daf64ef49e680bad63
HRISH-ABH <chauhanhrishabh51@gmail.com>
2025-08-13 22:47:52 +0530

    learnt about .on and .emit events in sockets Add custom emit and on events for generating AI reponses

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
-import httpServer from "./src/app.js"
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./src/app.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import generateResponse from "./src/service/ai.service.js";
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
 
+io.on("connection", (socket) => {
+  socket.on("message", (msg) => {
+    console.log(msg);
+  });
+
+  socket.on("ai-message", async (data) => {
+      console.log("Ai Prompt :", data.prompt);
+    const response = await generateResponse(data.prompt);
+    console.log("Ai response :", response);
+    socket.emit("ai-message-response",response);
+  });
+});
 httpServer.listen(3000, () => {
-    console.log("server is running on port 3000");
+  console.log("server is running on port 3000");
 });
---
+++ src/service/ai.service.js
@@ -0,0 +1,15 @@
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config();
+const ai = new GoogleGenAI({});
+
+async function generateResponse(prompt) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: prompt,
+  });
+
+  return response.text
+}
+
+export default generateResponse;
