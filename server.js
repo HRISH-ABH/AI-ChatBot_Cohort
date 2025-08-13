7face7c97cf9925b7fc3cbeb818b7478d6adb19b
HRISH-ABH <chauhanhrishabh51@gmail.com>
2025-08-13 23:34:20 +0530

    short term memory for our ai-client chats

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 import app from "./src/app.js";
 import { createServer } from "http";
@@ -9,17 +9,30 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   /* options */
 });
-
+const chatHistory = [];
 io.on("connection", (socket) => {
   socket.on("message", (msg) => {
     console.log(msg);
   });
 
   socket.on("ai-message", async (data) => {
-      console.log("Ai Prompt :", data.prompt);
-    const response = await generateResponse(data.prompt);
+    console.log("Ai Prompt :", data.prompt);
+
+    chatHistory.push({
+      role: "user",
+      parts: [{ text: data.prompt }],
+    });
+
+    const response = await generateResponse(chatHistory);
+    chatHistory.push({
+        role: "model",
+        parts: [{ text: response}],
+    });
+    for (let i = 0; i < chatHistory.length; i++) {
+         console.log(chatHistory[i].parts[0].text);
+       }
     console.log("Ai response :", response);
-    socket.emit("ai-message-response",response);
+    socket.emit("ai-message-response", response);
   });
 });
 httpServer.listen(3000, () => {
